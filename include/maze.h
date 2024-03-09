5dc0c9f6d2ce8f20541f5af56017fbb39a0b0bbf
ottyanna <ottyanna@gmail.com>
2024-03-09 17:09:50 +0100

    Updated adjacency matrix generation

---
+++ include/maze.h
@@ -0,0 +1,58 @@
+#ifndef MAZE_H
+#define MAZE_H // include guards
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+enum MazeEdgeType {
+
+  WALL, // zero as in adjacency matrix zero means no connection
+  OPEN
+
+};
+
+/*
+column index = adj
+1 2 3 4 5 6 7 8 9 .....
+. 1 . . 1 . . . . . . . 1 row index = node
+1 . 1 . . 1 . . . . . . 2
+. 1 . 1 . . 1 . . . . . 3
+. . 1 . . . . 1 . . . . 4
+1 . . . . 1 . . 1 . . .
+. 1 . . 1 . 1 . . 1 . .
+. . 1 . . 1 . 1 . . 1 .
+. . . 1 . . 1 . . . . 1
+. . . . 1 . . . . 1 . .
+. . . . . 1 . . 1 . 1 .
+. . . . . . 1 . . 1 . 1
+. . . . . . . 1 . . 1 .
+*/
+
+// Formato COO
+struct EdgeAdjMatrix {
+
+  int node;
+  int adj;
+  MazeEdgeType edgeType;
+
+  EdgeAdjMatrix(int _node, int _adj) {
+    node = _node;
+    adj = _adj;
+    edgeType = WALL; // in the beginning no node is connected
+  }
+
+  void print();
+};
+
+struct Maze {
+
+  vector<EdgeAdjMatrix> adjMatrix;
+  // manca vett di puntatori a nodi
+
+  void initGrid(int, int, int, int);
+  void print();
+};
+
+#endif
---
+++ src/maze.cpp
@@ -0,0 +1,37 @@
+#include "maze.h"
+
+void Maze::initGrid(int start, int finish, int sizeR,
+                    int sizeC) { // nodefault per sizeC?? //start e finish come
+                                 // li uso nel ptr???
+
+  // reserve space for the exact amount of symmetric edge
+  adjMatrix.reserve(sizeR * (sizeC - 1) + sizeC * (sizeR - 1));
+
+  // for the symmetric property is like going only to the right and down
+  for (int u = 0; u < sizeC * sizeR; u++) {
+    if ((u + 1) % sizeC != 0) { // cond non sono sul lato dx
+      adjMatrix.push_back(
+          EdgeAdjMatrix(u, u + 1)); // adiacenti destra e sinistra
+    }
+    if (u + sizeC < sizeC * sizeR)
+      adjMatrix.push_back(EdgeAdjMatrix(u, u + sizeC)); // adiacenti giù e su
+  }
+}
+
+void EdgeAdjMatrix::print() {
+
+  cout << endl;
+  cout << this->node << " ";
+  cout << this->adj;
+  cout << this->edgeType;
+  cout << "-----" << endl;
+}
+
+void Maze::print() {
+
+  for (auto elem : adjMatrix)
+    elem.print();
+
+  cout << endl;
+  cout << adjMatrix.size();
+}
